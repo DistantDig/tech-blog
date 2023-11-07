c3f2e02cff97c3170cbc48f7635514f852c94c7e
DistantDig <jameswaller711@gmail.com>
2023-11-06 22:52:16 -0800

    added get request for login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,3 +18,12 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+});
+
+module.exports = router;
